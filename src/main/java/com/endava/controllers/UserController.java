675c92b7c0ce02dbd8c928eab89c68c195d14515
Stanislav <sgirlea@endava.com>
2019-09-23 11:42:17 +0300

    created a user rest controller

---
+++ src/main/java/com/endava/controllers/UserController.java
@@ -0,0 +1,22 @@
+package com.endava.controllers;
+
+import com.endava.sevices.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity getAllUsers() {
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+}
