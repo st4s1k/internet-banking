11cc3d9c1eb6c5d346d4e5157d76dc67be6405a4
Stanislav <sgirlea@endava.com>
2019-10-04 20:18:30 +0300

    switched message properties to YAML

---
+++ src/main/java/com/endava/internship/internetbanking/config/Messages.java
@@ -0,0 +1,69 @@
+package com.endava.internship.internetbanking.config;
+
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Component
+@ConfigurationProperties("messages")
+public class Messages {
+
+    public Http http;
+    public Exceptions exceptions;
+
+    @Setter
+    public static class Http {
+
+        public User user;
+        public Account account;
+        public Transfer transfer;
+
+        @Setter
+        public static class User {
+
+            public Creation creation;
+
+            @Setter
+            public static class Creation {
+                public String success;
+                public String fail;
+                public String failExistingUsername;
+            }
+        }
+
+        @Setter
+        public static class Account {
+
+            public Creation creation;
+
+            @Setter
+            public static class Creation {
+                public String success;
+                public String fail;
+                public String userNotFound;
+            }
+        }
+
+        @Setter
+        public static class Transfer {
+            public String success;
+            public String currentAccountNotFound;
+            public String targetAccountNotFound;
+            public String invalidTransferAmount;
+            public String insufficientFunds;
+        }
+    }
+
+    @Setter
+    public static class Exceptions {
+
+        public Transfer transfer;
+
+        @Setter
+        public static class Transfer {
+            public String badSourceId;
+            public String badDestinationId;
+        }
+    }
+}
