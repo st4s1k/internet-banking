4c0f7628f0e8a970eebe90c550d4d2204dd4b181
Stanislav <sgirlea@endava.com>
2019-10-04 20:18:47 +0300

    switched message properties to YAML

--- src/main/java/com/endava/internship/internetbanking/InternetBanking.java
+++ src/main/java/com/endava/internship/internetbanking/InternetBanking.java
@@ -2,10 +2,8 @@ package com.endava.internship.internetbanking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("messages.properties")
 public class InternetBanking {
     public static void main(String[] args) {
         SpringApplication.run(InternetBanking.class, args);
