daa7584aaf6ae2ba0a775ea3229a5ff1944b2d23
Stanislav <st4s1k24@gmail.com>
2019-10-20 17:25:39 +0300

    - made banking service operations more time consuming, for tests

--- src/main/java/com/endava/internship/internetbanking/services/BankingService.java
+++ src/main/java/com/endava/internship/internetbanking/services/BankingService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class BankingService {
@@ -15,17 +16,29 @@ public class BankingService {
         this.transferService = transferService;
     }
 
+    public void refill(Long accountId, BigDecimal funds) {
+
+    }
+
     public void topUp(Long currentAccountId,
                       Long targetAccountId,
                       BigDecimal funds) {
-
+        try {
+            Thread.sleep(TimeUnit.SECONDS.toMillis(30));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         transferService.transfer(currentAccountId, targetAccountId, funds);
     }
 
     public void drawDown(Long currentAccountId,
                          Long targetAccountId,
                          BigDecimal funds) {
-
+        try {
+            Thread.sleep(TimeUnit.SECONDS.toMillis(30));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         transferService.transfer(targetAccountId, currentAccountId, funds);
     }
 }
