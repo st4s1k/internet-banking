41e456b1975001d337d36cbc2a27278409aed0ca
Stanislav <st4s1k24@gmail.com>
2019-10-27 20:05:34 +0200

    - improved log_balance_changes sql function

--- src/main/resources/db/migration/R__log_balance_changes_function.sql
+++ src/main/resources/db/migration/R__log_balance_changes_function.sql
@@ -2,7 +2,7 @@ create or replace function log_balance_changes()
     returns trigger as
 $body$
 begin
-   if tg_op = 'INSERT' or (tg_op = 'UPDATE' and new.funds <> old.funds) then
+   if tg_op = 'INSERT' or new.funds <> old.funds then
        insert into t_account_history(date_time, account_id, funds)
        values(now(), new.id, new.funds);
    end if;
