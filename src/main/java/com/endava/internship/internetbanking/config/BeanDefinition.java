2f5068f3ce577ced267e722727e028437316f3a4
Stanislav <sgirlea@endava.com>
2019-10-28 12:45:46 +0200

    - added logging to reconciliation service

--- src/main/java/com/endava/internship/internetbanking/config/BeanDefinition.java
+++ src/main/java/com/endava/internship/internetbanking/config/BeanDefinition.java
@@ -1,12 +1,24 @@
 package com.endava.internship.internetbanking.config;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
 
 @Configuration
 public class BeanDefinition {
 
-
-    // TODO https://github.com/eugenp/tutorials/tree/master/spring-boot-deployment/src/main/java/com/baeldung/gracefulshutdown44
-
-
+    @Bean
+    public Gson prettyPrintJSON() {
+        return new Gson().newBuilder()
+                .setPrettyPrinting()
+                .disableHtmlEscaping()
+                .enableComplexMapKeySerialization()
+                .registerTypeAdapter(LocalDateTime.class, (JsonSerializer<LocalDateTime>)
+                        (date, typeOfSrc, context) -> new JsonPrimitive(date.toString()))
+                .create();
+    }
 }
--- src/main/java/com/endava/internship/internetbanking/services/ReconciliationService.java
+++ src/main/java/com/endava/internship/internetbanking/services/ReconciliationService.java
@@ -5,14 +5,18 @@ import com.endava.internship.internetbanking.config.Messages;
 import com.endava.internship.internetbanking.entities.Account;
 import com.endava.internship.internetbanking.entities.AccountSnapshot;
 import com.endava.internship.internetbanking.entities.Transfer;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.math.BigDecimal.ZERO;
 import static java.time.LocalDateTime.now;
@@ -28,16 +32,19 @@ public class ReconciliationService {
     private final TransferService transferService;
     private final Messages.Logging.Reconciliation msg;
     private final InternetBankingEnv.Reconciliation env;
+    private final Gson gson;
 
     @Autowired
     public ReconciliationService(AccountSnapshotService accountSnapshotService,
                                  TransferService transferService,
                                  Messages msg,
-                                 InternetBankingEnv env) {
+                                 InternetBankingEnv env,
+                                 @Qualifier("prettyPrintJSON") Gson gson) {
         this.accountSnapshotService = accountSnapshotService;
         this.transferService = transferService;
         this.msg = msg.logging.reconciliation;
         this.env = env.reconciliation;
+        this.gson = gson;
     }
 
     @Scheduled(fixedRateString = "${internetbanking.env.reconciliation.rate}")
@@ -46,7 +53,16 @@ public class ReconciliationService {
         log.info(msg.started);
 
         LocalDateTime lastReconciliationTime = now().minus(env.rate, MILLIS);
+        log.debug("lastReconciliationTime: {}", lastReconciliationTime.toString());
+
         List<Transfer> transfers = transferService.findAllAfter(lastReconciliationTime);
+        log.debug("transfers:\n{}", gson.toJson(transfers));
+
+        LocalDateTime firstTransferAfterLastReconciliation = transfers.stream()
+                .min(Comparator.comparing(Transfer::getDateTime))
+                .map(Transfer::getDateTime)
+                .orElse(lastReconciliationTime);
+        log.debug("firstTransferAfterLastReconciliation: {}", firstTransferAfterLastReconciliation.toString());
 
         if (!transfers.isEmpty()) {
 
@@ -54,28 +70,39 @@ public class ReconciliationService {
 
             Map<Account, List<Transfer>> sourceGroups = transfers.parallelStream()
                     .collect(groupingBy(Transfer::getSourceAccount));
+            log.debug("sourceGroups:\n{}", gson.toJson(sourceGroups));
 
             Map<Account, List<Transfer>> destinationGroups = transfers.parallelStream()
                     .collect(groupingBy(Transfer::getDestinationAccount));
+            log.debug("destinationGroups:\n{}", gson.toJson(destinationGroups));
 
-            Set<Account> accounts = new HashSet<>();
-            accounts.addAll(sourceGroups.keySet());
-            accounts.addAll(destinationGroups.keySet());
+            Set<Account> accounts = transfers.stream()
+                    .flatMap(t -> Stream.of(t.getSourceAccount(), t.getDestinationAccount()))
+                    .collect(Collectors.toSet());
+            log.debug("accounts:\n{}", gson.toJson(accounts));
 
             List<AccountSnapshot> snapshots =
-                    accountSnapshotService.findAllEarliestAfter(accounts, lastReconciliationTime);
+                    accountSnapshotService.findAllLatestBefore(accounts, firstTransferAfterLastReconciliation);
+            log.debug("snapshots:\n{}", gson.toJson(snapshots));
 
             balanceConsistencyIsOk = snapshots.size() >= transfers.size();
+            log.debug("snapshots.size() >= transfers.size(): {}", balanceConsistencyIsOk);
 
             if (balanceConsistencyIsOk) {
 
                 Map<Account, BigDecimal> lossMap = reduceTransfers(sourceGroups);
+                log.debug("lossMap:\n{}", gson.toJson(lossMap));
+
                 Map<Account, BigDecimal> gainMap = reduceTransfers(destinationGroups);
+                log.debug("gainMap:\n{}", gson.toJson(gainMap));
 
                 Map<Account, BigDecimal> expectedDifferential =
                         calculateBalanceDifferentialRegardingTransferHistory(accounts, lossMap, gainMap);
+                log.debug("expectedDifferential:\n{}", gson.toJson(expectedDifferential));
+
                 Map<Account, BigDecimal> actualDifferential =
                         calculateBalanceDifferentialRegardingAccountHistory(accounts, snapshots);
+                log.debug("actualDifferential:\n{}", gson.toJson(actualDifferential));
 
                 balanceConsistencyIsOk = actualDifferential.entrySet().stream()
                         .map(entry -> expectedDifferential.get(entry.getKey())
@@ -109,8 +136,9 @@ public class ReconciliationService {
                 account -> snapshots.stream()
                         .filter(s -> s.getAccount().equals(account))
                         .findAny()
-                        .map(snapshot -> snapshot.getFunds().subtract(account.getFunds()))
-                        .orElse(ZERO)));
+                        .map(AccountSnapshot::getFunds)
+                        .orElse(ZERO)
+                        .subtract(account.getFunds())));
     }
 
     private Map<Account, BigDecimal>
